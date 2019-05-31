6aced26fe2b67fbffd581a327eda6bd6f15454f5
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 22:40:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,18 @@
 class Triangle
 
-  attr_reader :side1, :side2, :side3
+  attr_reader :side1, :side2, :side3, :sides
 
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    @sides [side1, side2, side3]
   end
 
   def kind
+    if sides.detect {|side| <= 0}
+      raise TriangleError
+    end
   end
 
   class TriangleError < StandardError
